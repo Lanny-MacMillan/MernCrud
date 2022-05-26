7074e30eedc3b4714ed98623a29b01d16995b881
PORTORREAL <portorreal@ad25wv020h7jy.myfiosgateway.com>
2022-05-26 12:19:50 -0400

    display form to add new item to array

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,22 +1,22 @@
 import './App.css';
 import {useEffect, useState} from 'react';
 import axios from 'axios';
-import Animal from './components/Animal'
+import Food from './components/food.js'
 
 
 function App() {
-
-
-
-
-
-
-
   return (
-    <>
-    <h1>Food</h1>
-    
-    </>
+    <div>
+      <h1>Food</h1>
+      <section>
+        <h2>Add a New Food to Our Menu</h2>
+        <form onSubmit={handleNewFoodFormSubmit}>
+          Name: <input type="text" onChange={handleNewName}/><br/>
+          Image Link: <input type="text" onChange={handleNewImage}/><br/>
+          <input type="submit" value="Create New Food Listing"/>
+        </form>
+      </section>
+    </div>
   );
 }
 
