4d93223f847213921bc88219676302cb9dd753f2
Lanny Mac <Lanny@MacBook-Pro.local>
2022-05-26 11:38:20 -0400

    front_end setup

---
+++ front_end/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+import Animal from './components/Animal'
+
+
+function App() {
+
+
+
+
+
+
+
+  return (
+    <>
+    <h1>Food</h1>
+    
+    </>
+  );
+}
+
+export default App;
